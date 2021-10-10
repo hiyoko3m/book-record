ab224d74be2452b71525badcc9fef6c6e5dfd247
hyksm <hiyoko3m.tech@gmail.com>
2021-10-10 16:33:52 +0900

    update: main.rsの余計なコードを削除

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,17 +1,34 @@
+mod models;
+mod schema;
+
+// 本当はrocket_sync_db_poolsの中のdieselと被るので
+// extern crateしたくないのだが、
+// schema.rsのコンパイルを通すときには現実的な解はこれしかなさそう
+#[macro_use]
+extern crate diesel;
+
+use dotenv::dotenv;
+use rocket::serde::json::Json;
 use rocket::{get, launch, routes};
-use rocket_sync_db_pools::{database, diesel};
+use rocket_diesel::{QueryDsl, RunQueryDsl};
+use rocket_sync_db_pools::{database, diesel as rocket_diesel};
 
-#[database("sqlite_book_record")]
-struct BookRecordDbConn(diesel::SqliteConnection);
+use self::models::Book;
 
-/// pepepe
+#[database("db_book_record")]
+struct BookRecordDbConn(rocket_diesel::MysqlConnection);
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+async fn index(conn: BookRecordDbConn) -> Json<Option<Book>> {
+    use self::schema::books::dsl::*;
+
+    let res = conn.run(|c| books.limit(1).load::<Book>(c)).await;
+    Json(res.ok().map(|mut v| v.pop()).flatten())
 }
 
 #[launch]
 fn app() -> _ {
+    dotenv().ok();
     rocket::build()
         .attach(BookRecordDbConn::fairing())
         .mount("/", routes![index])
