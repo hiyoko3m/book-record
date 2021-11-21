95e584295ad6ec057409f6af965cf0eb2f9330ce
hiyoko3m <hiyoko3m.tech@gmail.com>
2021-11-21 15:53:31 +0900

    feat: update axum version to 0.3.4

--- backend/src/controller/book.rs
+++ backend/src/controller/book.rs
@@ -1,10 +1,8 @@
 use axum::{
-    extract::{self, Path},
-    handler::{get, post},
+    extract::Path,
     http::StatusCode,
-    response,
-    routing::BoxRoute,
-    Router,
+    routing::{get, post},
+    Json, Router,
 };
 use serde_json::{json, Value};
 
@@ -12,7 +10,7 @@ use super::models::BookExtract;
 use crate::domain::entity::book::BookEntity;
 use crate::domain::service::book::BookService;
 
-pub fn book_app() -> Router<BoxRoute> {
+pub fn book_app() -> Router {
     Router::new()
         .route("/books", get(list_books))
         .route("/book", post(create_book))
@@ -20,12 +18,11 @@ pub fn book_app() -> Router<BoxRoute> {
             "/book/:id",
             get(get_book).post(update_book).delete(delete_book),
         )
-        .boxed()
 }
 
-async fn list_books(book_service: BookService) -> response::Json<Value> {
+async fn list_books(book_service: BookService) -> Json<Value> {
     let books: Vec<BookEntity> = book_service.list_books().await;
-    response::Json(json!({
+    Json(json!({
         "books": books,
     }))
 }
@@ -33,10 +30,10 @@ async fn list_books(book_service: BookService) -> response::Json<Value> {
 async fn get_book(
     book_service: BookService,
     Path(book_id): Path<u32>,
-) -> Result<response::Json<Value>, StatusCode> {
+) -> Result<Json<Value>, StatusCode> {
     let book: Option<BookEntity> = book_service.get_book(book_id).await;
     book.map(|book| {
-        response::Json(json!({
+        Json(json!({
             "book": book,
         }))
     })
@@ -45,12 +42,12 @@ async fn get_book(
 
 async fn create_book(
     book_service: BookService,
-    extract::Json(payload): extract::Json<BookExtract>,
-) -> Result<response::Json<Value>, StatusCode> {
+    Json(payload): Json<BookExtract>,
+) -> Result<Json<Value>, StatusCode> {
     let book_id = book_service.create_book(payload.into()).await;
     book_id
         .map(|book_id| {
-            response::Json(json!({
+            Json(json!({
                 "book_id": book_id,
             }))
         })
@@ -60,7 +57,7 @@ async fn create_book(
 async fn update_book(
     book_service: BookService,
     Path(book_id): Path<u32>,
-    extract::Json(payload): extract::Json<BookExtract>,
+    Json(payload): Json<BookExtract>,
 ) -> StatusCode {
     let result = book_service.update_book((book_id, payload).into()).await;
     if result {
