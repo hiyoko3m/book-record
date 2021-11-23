6532d909da6b60f18cb6547755cf672c391a3e9d
hiyoko3m <hiyoko3m.tech@gmail.com>
2021-11-23 17:53:17 +0900

    feat: 設定変数を環境変数から読み込む仕組みの構築

--- backend/src/controller/models.rs
+++ backend/src/controller/models.rs
@@ -2,16 +2,23 @@ use serde::Deserialize;
 
 use crate::domain::entity::user::{SignUpCode, UserEntityForCreation};
 
+#[derive(Debug, Deserialize, Clone)]
 pub struct Settings {
+    pub database_url: String,
+
+    // OpenID Connectのprovider
+    // 最後にslashを入れてはいけない
+    pub id_provider_url: String,
+    pub id_provider_client_id: String,
+    pub id_provider_client_secret: String,
+    pub id_provider_redirect_url: String,
+
+    #[serde(default = "default_refresh_key")]
     pub refresh_token_cookie_name: String,
 }
 
-impl Default for Settings {
-    fn default() -> Self {
-        Self {
-            refresh_token_cookie_name: "refresh_token".to_string(),
-        }
-    }
+fn default_refresh_key() -> String {
+    "refresh_token".to_string()
 }
 
 #[derive(Debug, Deserialize)]
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -7,6 +7,7 @@ use std::net::SocketAddr;
 use std::sync::Arc;
 
 use axum::{AddExtensionLayer, Router};
+use dotenv::dotenv;
 use openidconnect::core::{CoreClient, CoreProviderMetadata};
 use openidconnect::reqwest::async_http_client;
 use openidconnect::{AuthType, ClientId, ClientSecret, IssuerUrl, RedirectUrl};
@@ -18,42 +19,58 @@ use self::controller::{book::book_app, user::user_app};
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
+    tracing::info!("initialization start");
 
-    let pool = PgPool::connect("postgres://hyo:hyo@localhost/book_record")
-        .await
-        .unwrap();
+    // .envファイル読み込み
+    dotenv().ok();
 
-    let shared_settings = Arc::new(Settings::default());
+    // 設定変数の初期化
+    let settings = envy::from_env::<Settings>().expect(
+        "initialization error: failed in constructing app's settings from environment variables",
+    );
 
+    // repository層の外部アクセス先の初期化
+    let pool = PgPool::connect(&settings.database_url).await.unwrap();
+
+    // IdPの設定初期化
     let provider_metadata = CoreProviderMetadata::discover_async(
-        IssuerUrl::new("https://accounts.google.com".to_string())
-            .expect("Error on constructing issuer url"),
+        IssuerUrl::new(settings.id_provider_url.to_owned())
+            .expect("initialization error: failed in parsing the URL of IdP"),
         async_http_client,
     )
     .await
-    .expect("Error on constucting the provider metadata");
+    .expect("initialization error: failed in discovering the IdP's metadata");
 
     let client = CoreClient::from_provider_metadata(
         provider_metadata,
-        ClientId::new(
-            "CLIENT_ID".to_string(),
-        ),
-        Some(ClientSecret::new("CLIENT_SECRET".to_string())),
+        ClientId::new(settings.id_provider_client_id.to_owned()),
+        Some(ClientSecret::new(
+            settings.id_provider_client_secret.to_owned(),
+        )),
     )
-    .set_redirect_uri(RedirectUrl::new("http://localhost:8000".to_string()).expect("a"));
+    .set_redirect_uri(
+        RedirectUrl::new(settings.id_provider_redirect_url.to_owned()).expect(
+            "initialization error: failed in parsing the redirect URL which be passed to IdP",
+        ),
+    );
 
+    // axumのアプリケーション構築
     let app = Router::new().nest(
         "/v1",
         Router::new()
             .merge(book_app())
             .merge(user_app())
-            .layer(AddExtensionLayer::new(shared_settings))
+            .layer(AddExtensionLayer::new(Arc::new(settings)))
             .layer(AddExtensionLayer::new(client))
             .layer(AddExtensionLayer::new(pool)),
     );
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
-    tracing::info!("listening on {}", addr);
+
+    tracing::info!(
+        "initialization complete; now the server is listening on {}",
+        addr
+    );
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
